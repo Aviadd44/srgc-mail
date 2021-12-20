9ade66f3a8ec372788e965ddf09fc378cd246b8f
Aviadd44 <92351158+Aviadd44@users.noreply.github.com>
2021-12-20 14:33:27 +0530

    Update DDL

--- structure.sql
+++ structure.sql
@@ -1,10 +1,8 @@
-CREATE TABLE IF NOT EXISTS `mydb`.`SRGC` (
+CREATE TABLE IF NOT EXISTS `srgc`.`student` (
   `firstName` VARCHAR(45) NOT NULL,
   `lastName` VARCHAR(45) NOT NULL,
-  `address` VARCHAR(45) NOT NULL,
   `username` VARCHAR(45) NOT NULL,
-  `emailId` VARCHAR(45) NOT NULL)
-ENGINE = InnoDB;
-
-ALTER TABLE `mydb`.`SRGC` 
-RENAME TO  `mydb`.`student` ;
+  `address` VARCHAR(45) NOT NULL,
+  `emailId` VARCHAR(45) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+   PRIMARY KEY (`id`));
